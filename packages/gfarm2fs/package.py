56c5ed3f58b3fb728556578537716f6b84a66e7b
TAKAHASHI Shuuji <shuuji3@gmail.com>
2020-10-22 07:30:00 +0900

    Update gfarm2fs package.

--- packages/gfarm2fs/package.py
+++ packages/gfarm2fs/package.py
@@ -1,54 +1,36 @@
 # Copyright 2013-2019 Lawrence Livermore National Security, LLC and other
 # Spack Project Developers. See the top-level COPYRIGHT file for details.
 #
-# SPDX-License-Identifier: (Apache-2.0 OR MIT)
-
-# ----------------------------------------------------------------------------
-# If you submit this package back to Spack as a pull request,
-# please first remove this boilerplate and all FIXME comments.
-#
-# This is a template package file for Spack.  We've put "FIXME"
-# next to all the things you'll want to change. Once you've handled
-# them, you can save this file and test your package like this:
-#
-#     spack install gfarm2fs
-#
-# You can edit this file again by typing:
-#
-#     spack edit gfarm2fs
-#
-# See the Spack documentation for more information on packaging.
-# ----------------------------------------------------------------------------
+# SPDX-License-Identifier: MIT
 
 from spack import *
 
 
 class Gfarm2fs(AutotoolsPackage):
-    """A plugin to mount gfarm filesystem"""
+    """Linux FUSE client of Gfarm"""
 
-    # FIXME: Add a proper url for your package's homepage here.
-    homepage = "http://oss-tsukuba.org/software/gfarm"
-    url = "https://sourceforge.net/projects/gfarm/files/gfarm2fs/1.2.12/gfarm2fs-1.2.12.tar.gz"
+    homepage = 'http://oss-tsukuba.org/software/gfarm'
+    url = 'https://github.com/oss-tsukuba/gfarm2fs/archive/1.2.14.tar.gz'
+    git = 'https://github.com/oss-tsukuba/gfarm2fs.git'
 
-    version('develop', svn='https://svn.code.sf.net/p/gfarm/code/gfarm2fs/trunk', preferred=True)
-    version('1.2.12', sha256='e61d6898d58c32f96d989b573ac0356068f174516be7659c33e603e160fbab8b')
+    version('main', branch='master', preferred=True)
+    version('1.2.14', sha256='1263c810a8dce8e2c1bf8fa6864aae0f48b824c6')
 
-    # FIXME: Add dependencies if required.
     depends_on('gfarm')
+    # depends_on('libfuse@2.9.9')
 
-    variant(
-         'acl', default=True, description='Builds with acl'
-     )
+    variant('acl', default=False, description='Builds with acl')
+
+    if '+acl' in spec:
+        depends_on('acl')
 
     def configure_args(self):
-        # FIXME: Add arguments other than --prefix
-        # FIXME: If not needed delete this function
         spec = self.spec
         args = []
 
-        if not '+acl' in spec:
+        if '+acl' not in spec:
             args.append('--disable-acl')
 
-        args.append('--with-gfarm={0}'.format(spec['gfarm'].prefix))
+        args.append('--with-gfarm={prefix}'.format(prefix=spec['gfarm'].prefix))
 
         return args
